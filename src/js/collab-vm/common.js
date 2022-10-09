7e78640fab5ba5fe510704a803e1db56eb361bc5
Jacob <osjacobtimer@gmail.com>
2022-10-09 10:01:29 +0100

    Main files added
    As the title says, all the files that were needed, are here.

---
+++ src/js/collab-vm/common.js
@@ -0,0 +1,50 @@
+// CollabVM webapp configuration
+
+const root="/collab-vm/";
+
+export default {
+	DEBUG: false,
+	DEBUG_NO_TIMEOUT: false,
+	DEBUG_NO_NSFW: false,
+	DEBUG_NO_CONNECT: false, // is this used?
+	DEBUG_VM_LIST: false,
+	DEBUG_VM_LIST: false,
+
+	debugLog: function() {
+		if(this.DEBUG)
+			console.log.apply(null, arguments);
+	},
+
+	/** @const
+	 * The root directory of the collab-vm project with a
+	 * forward slash appended to it.
+	 * This is determined at runtime to allow the project to be
+	 * relocated without needing to recompile the javascript.
+	 * @type {string}
+	 */
+	rootDir: root,
+	chatSound: root + "notify",
+
+	/**
+	 * The main node this webapp is configured to connect to.
+	 * @const
+	 */
+	serverAddress: "localhost:6004",
+
+	/**
+	 * Additional nodes to connect to.
+	 * Uses multicollab() to do so
+	 * @const
+	 */
+	additionalNodes: [
+	],	
+	
+	/**
+	 * Additional secure nodes to connect to.
+	 * Uses multicollab(%s, true) to do so
+	 * @const
+	 */
+	 
+	additionalNodesSecure: [
+	]
+};
